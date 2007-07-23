b6736ca199dd64497eea28078c3d25653e004348
Priyank <priyank@frugalware.org>
2007-07-23 19:43:09 +0530

    gfpm: new source file gfpm-util.c,h * contains utility functions like gfpm_bold which returns bold text (markup'ed)

---
+++ src/gfpm-util.c
@@ -0,0 +1,29 @@
+/*
+ *  gfpm-util.c for gfpm
+ *
+ *  Copyright (C) 2007 by Priyank Gosalia <priyankmg@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include "gfpm-util.h"
+
+char *
+gfpm_bold (const char *text)
+{
+	if (text == NULL)
+		return NULL;
+	return ((char*)g_markup_printf_escaped("<b>%s</b>",text));
+}
---
+++ src/gfpm-util.h
@@ -0,0 +1,15 @@
+#ifndef __GFPM_UTIL_H__
+#define __GFPM_UTIL_H__
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <locale.h>
+#include <libintl.h>
+#include <gtk/gtk.h>
+#include "gfpm.h"
+
+char * gfpm_bold (const char *);
+
+#endif
