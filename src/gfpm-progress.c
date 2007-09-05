0bad3214a044bd6c24402877afc1fdf9f4f659c4
Priyank <priyank@frugalware.org>
2007-09-05 21:57:10 +0530

    gfpm-progress: whitespace fix

--- src/gfpm-progress.c
+++ src/gfpm-progress.c
@@ -250,7 +250,6 @@ gfpm_progress_event (unsigned char event, void *data1, void *data2)
 			break;
 		case PM_TRANS_EVT_SCRIPTLET_START:
 			substr = g_strdup ((char*)data1);
-			
 			break;
 		case PM_TRANS_EVT_SCRIPTLET_DONE:
 			substr = g_strdup (_("Done"));
