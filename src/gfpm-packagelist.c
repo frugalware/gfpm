b5e284d94ac97f46b7a1fa3d50bfed3dff0b16c0
Priyank <priyank@frugalware.org>
2007-09-19 19:30:10 +0530

    gfpm-packagelist: modified license notice

--- src/gfpm-packagelist.c
+++ src/gfpm-packagelist.c
@@ -1,10 +1,8 @@
-/***************************************************************************
- *  gfpm-packagelist.c
- *  Author: Priyank Gosalia <priyankmg@gmail.com>
- *  Copyright 2006-2007 Frugalware Developer Team
- ****************************************************************************/
-
 /*
+ *  gfpm-packagelist.c for gfpm
+ *
+ *  Copyright (C) 2006-2007 by Priyank Gosalia <priyankmg@gmail.com>
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
